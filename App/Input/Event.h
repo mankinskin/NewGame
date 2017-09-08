6b42a8b5959c2471478c69275f7fb55bd85ca273
overestimator <linusbehrbohm@web.de>
2017-09-08 16:50:59 +0200

    reorganized Event.h

--- App/Input/Event.h
+++ App/Input/Event.h
@@ -4,56 +4,12 @@
 #include <initializer_list>
 namespace App {
 	namespace Input {
-		
-		extern std::vector<int> allSignalLocks;
-		extern std::vector<unsigned> allSignalSlots;
-		extern std::vector<unsigned> allSignals;//for ordered access
-		extern unsigned TOTAL_SIGNAL_COUNT;
-
-		
-		template<class EventType>
-		class EventSlot {
-		public:
-			static unsigned int create(EventType pEvent) {
-				instances.push_back(EventSlot<EventType>(pEvent));
-				return instances.size() - 1;
-			}
-			static unsigned int instance_count() {
-				return instances.size();
-			}
-
-			static EventSlot<EventType>& get(unsigned int index) {
-				return instances[index];
-			}
-			EventType evnt;
-			unsigned index;
-		private:
-			EventSlot(EventType pEvent) :index(TOTAL_SIGNAL_COUNT++), evnt(pEvent) {}
-			
-			static std::vector<EventSlot<EventType>> instances;
-		};
-		template<class EventType>
-		std::vector<EventSlot<EventType>> EventSlot<EventType>::instances = std::vector<EventSlot<EventType>>();
-		
-		extern std::unordered_map<unsigned int, std::pair<int, std::vector<unsigned int>>> signalLockBindings;
-		
-		static void signal_lock(unsigned int pLockSignal, std::initializer_list<unsigned int> pSignal, int pLock = 1) {
-			auto it = signalLockBindings.find(pLockSignal);
-			if (it == signalLockBindings.end()) {
-				signalLockBindings.insert(std::pair<unsigned int, std::pair<int, std::vector<unsigned int>>>(pLockSignal, std::pair<int, std::vector<unsigned int>>(pLock, pSignal)));
-				return;
-			}
-			it->second.second.insert(it->second.second.end(), pSignal.begin(), pSignal.end());
-		}
-		static void signal_unlock(unsigned int pLockSignal, std::initializer_list<unsigned int> pSignal) {
-			signal_lock(pLockSignal, pSignal, 0);
-		}
 		template<typename R, typename... Args>
 		class FuncSlot {
 		private:
 			R(*fun)(Args...);
 			std::tuple<Args...> args;
-			
+
 			unsigned int slot_index;
 		public:
 			FuncSlot() : fun(nullptr), args(std::tuple<Args...>()), signal_bindings(std::vector<unsigned>())
@@ -87,5 +43,49 @@ namespace App {
 		};
 		template<typename R, typename... Args>
 		std::vector<FuncSlot<R, Args...>> FuncSlot<R, Args...>::instances = std::vector<FuncSlot<R, Args...>>();
+
+		template<class EventType>
+		class EventSlot {
+		public:
+			static unsigned int create(EventType pEvent) {
+				instances.push_back(EventSlot<EventType>(pEvent));
+				return instances.size() - 1;
+			}
+			static unsigned int instance_count() {
+				return instances.size();
+			}
+
+			static EventSlot<EventType>& get(unsigned int index) {
+				return instances[index];
+			}
+			EventType evnt;
+			unsigned index;
+		private:
+			EventSlot(EventType pEvent) :index(TOTAL_SIGNAL_COUNT++), evnt(pEvent) {}
+
+			static std::vector<EventSlot<EventType>> instances;
+		};
+		template<class EventType>
+		std::vector<EventSlot<EventType>> EventSlot<EventType>::instances = std::vector<EventSlot<EventType>>();
+
+
+		extern std::vector<int> allSignalLocks;
+		extern std::vector<unsigned> allSignalSlots;
+		extern std::vector<unsigned> allSignals;//for ordered access
+		extern unsigned TOTAL_SIGNAL_COUNT;
+		extern std::unordered_map<unsigned int, std::pair<int, std::vector<unsigned int>>> signalLockBindings;
+		
+		static void signal_lock(unsigned int pLockSignal, std::initializer_list<unsigned int> pSignal, int pLock = 1) {
+			auto it = signalLockBindings.find(pLockSignal);
+			if (it == signalLockBindings.end()) {
+				signalLockBindings.insert(std::pair<unsigned int, std::pair<int, std::vector<unsigned int>>>(pLockSignal, std::pair<int, std::vector<unsigned int>>(pLock, pSignal)));
+				return;
+			}
+			it->second.second.insert(it->second.second.end(), pSignal.begin(), pSignal.end());
+		}
+		static void signal_unlock(unsigned int pLockSignal, std::initializer_list<unsigned int> pSignal) {
+			signal_lock(pLockSignal, pSignal, 0);
+		}
+		
 	}
 }
